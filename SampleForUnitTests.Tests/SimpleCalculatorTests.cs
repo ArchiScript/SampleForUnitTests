9b8dee8d9117391e382f2c5e3d3429940c2cef0b
Артур <arturskomarohov@gmail.com>
2021-08-23 18:35:14 +0300

    tests

---
+++ SampleForUnitTests.Tests/SimpleCalculatorTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SampleForUnitTests.Tests
+{
+    public class SimpleCalculatorTests
+    {
+        [Fact]
+        public void GetSum_2_Plus_5_Eq_7()
+        {
+            //Arrange
+            SimpleCalculator calculator = new SimpleCalculator();
+            //Act 
+            var result = calculator.GetSum(2, 5);
+
+            //Assert;
+            Assert.Equal(7, result);
+        }
+        [Fact]
+
+        public void GetSubstruct_5_Min_2_Eq_3() {
+        // Arrange
+            SimpleCalculator calculator = new SimpleCalculator();
+            //Act 
+            var result = calculator.GetSubstract(2, 5);
+
+            //Assert;
+            Assert.Equal(7, result);
+        }
+    }
+}
